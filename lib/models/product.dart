5d73b778a3383ba00729438a9081aac7940df490
Ontan8 <tanishqpawar80@gmail.com>
2022-01-31 23:34:20 +0530

    product's data model

---
+++ lib/models/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool
+      isFavorite; //this is not final because it may change at runtime as a user can favorite or unfavorite a product at runtime.
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.imageUrl,
+      this.isFavorite = false});
+}
