af89143a92e409175077eebf3704c4166709fea6
Ontan8 <tanishqpawar80@gmail.com>
2022-02-06 12:08:18 +0530

    cartitem provider model

---
+++ lib/providers/cart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/widgets.dart';
+
+class CartItem {
+  final String title;
+  final String id;
+  final int quantity;
+  final double price;
+  CartItem(
+      {required this.id,
+      required this.price,
+      required this.quantity,
+      required this.title});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (value) => CartItem(
+              id: value.id,
+              price: value.price,
+              quantity: value.quantity + 1,
+              title: value.title));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              price: price,
+              quantity: 1,
+              title: title));
+    }
+  }
+}
