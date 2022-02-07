be4bf4c902ec14322db0b0cbe8ceee587a1237fd
Ontan8 <tanishqpawar80@gmail.com>
2022-02-07 19:22:06 +0530

    routes for appdrawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Orders;
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders-screen';
   @override
   Widget build(BuildContext context) {
     final ordersData = Provider.of<Orders>(context);
     return Scaffold(
+      drawer: AppDrawer(),
       appBar: AppBar(title: const Text('Your Orders')),
       body: ListView.builder(
         itemBuilder: ((context, index) => OrderItem(ordersData.orders[index])),
