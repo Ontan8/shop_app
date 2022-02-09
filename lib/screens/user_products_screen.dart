06fde4300c89ceed8a1f37fc5d24d1160069ff02
Ontan8 <tanishqpawar80@gmail.com>
2022-02-09 17:08:29 +0530

    userproddata screen and widget

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
+
+import '../providers/products_provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  const UserProductsScreen({Key? key}) : super(key: key);
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (ctx, index) => Column(
+            children: [
+              UserProductItem(
+                  title: productsData.items[index].title,
+                  imageUrl: productsData.items[index].imageUrl),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
